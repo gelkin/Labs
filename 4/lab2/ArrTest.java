aeef94975ea4cdd383a14bdecddb530d625eeed9
gelkin <vladimir@vladimir-Lenovo-G570.(none)>
2013-05-21 03:33:13 +0400

    Forth lab is loaded

---
+++ 4/lab2/ArrTest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class ArrTest {
+  public static void main(String[] args){
+ 	ArrayList<String>[] arr = new ArrayList[10];
+    /*if (map[2] == null) {
+    	System.out.println("EEEEEHUUU");
+    }*/
+  }
+}
